bfbc37f49e7ce1fa7cf5ce5de2f5c9d7e722e199
Ylenia <ylenia.icmr@gmail.com>
2021-09-23 01:06:32 +0200

    random img

--- memoryGame/main.js
+++ memoryGame/main.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-let nbrRow = 2
-let nbrCol = 3;
+    let nbrRow = 2
+    let nbrCol = 3;
+
+    let pix = new Array("img/ronaldo-minion.jpg", "img/messi-minion.jpg", "img/rio.jpg");
+
+
     createColRow(nbrRow, nbrCol);
+
+    choosePix(pix)
 });
 
 
 let table = document.getElementById('memoryTable');
+
 // let table = document.querySelector('memoryTable');
 
 
@@ -18,12 +25,37 @@ function createColRow(nbrRow, nbrCol) {
 
         for (let j = 0; j < nbrCol; j++) {
             let col = document.createElement('td');
+            // col.setAttribute('class', 'col-td');
+            col.classList.add('col-td');
             row.appendChild(col);
         }
         table.appendChild(row);
     }
 }
 
-function showImg() {
-    int img = ((int)(Math.random() * 4) + 1);
+function inArray(item, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (item == array[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function deleteItem(ingRand, imgArray) {
+    for (var i = imgArray.length - 1; i >= 0; i--) {
+        if (imgArray[i] === ingRand  ) {
+            imgArray.splice(i, 1);
+        }
+    }
+}
+
+function choosePix(imgArray) {
+    let arraySave = [];
+    document.querySelectorAll('.col-td').forEach(td => {
+        let imgRand = imgArray[Number(Math.floor(Math.random() * imgArray.length))];
+        td.innerHTML = `<img src="${imgRand}" alt="img">` ;
+
+        !inArray(imgRand, arraySave) ?  arraySave.push(imgRand) :  deleteItem(imgRand, imgArray);
+    })
 }
