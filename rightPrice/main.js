dd2754a8e7a4f358b31ae0aef032cd4816bc97ae
Ylenia <ylenia.icmr@gmail.com>
2021-09-23 10:19:09 +0200

    simplify rightPrice

--- rightPrice/main.js
+++ rightPrice/main.js
@@ -1,19 +1,26 @@
 let price = Math.floor(Math.random()* (1000 - 300 +1)) + 300;
 alert(price)
 
-let nbrInput = null;
+let nbrInput;
 
-while ( nbrInput != price){
+// while ( ){
+//     nbrInput = parseInt(prompt("Enter a value between 300 & 1000"));
+//
+//     if (nbrInput < price ){
+//         alert("It's more")
+//     }
+//     else if (nbrInput > price){
+//         alert("It's less")
+//     }
+// }
+//
+// if (nbrInput == price){
+//     alert("You win !!!")
+// }
+
+do {
     nbrInput = parseInt(prompt("Enter a value between 300 & 1000"));
+    nbrInput > price ?  alert("It's less") : alert("It's more");
+}while (nbrInput != price)
 
-    if (nbrInput < price ){
-        alert("It's more")
-    }
-    else if (nbrInput > price){
-        alert("It's less")
-    }
-}
-
-if (nbrInput == price){
-    alert("You win !!!")
-}
+alert("You win !!!");
