0d41f43628dca2fdaa7b558d99a8a8520e041e3a
Ylenia <ylenia.icmr@gmail.com>
2021-10-30 20:50:44 +0200

    show img

--- memoryGame/main.js
+++ memoryGame/main.js
@@ -1,21 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+// array picture
+// let imgArray = new Array("./img/koala.jpg", "./img/agneau.jpg", "./img/rinho.jpg");
+let imgArray = ["./img/koala.jpg", "./img/agneau.jpg", "./img/rinho.jpg", "./img/monkey.jpg", "./img/tiger.jpg"];
+let arraySave = [];
 
-    let nbrRow = 2
-    let nbrCol = 3;
+// let btnPlay = document.querySelector('.btnPlay');
+// btnPlay.addEventListener("click", function (){
+//     choosePix(imgArray)
+// })
 
-    let pix = new Array("./img/koala.jpg", "./img/agneau.jpg", "./img/rinho.jpg");
+const nbrRow = 2
+const nbrCol = 3;
 
-    play(nbrRow, nbrCol, pix);
-    createColRow(nbrRow, nbrCol);
-    // choosePix(pix)
-
-
-});
-
+//create grid
 let table = document.getElementById('memoryTable');
-
-// let table = document.querySelector('memoryTable');
-
 
 function createColRow(nbrRow, nbrCol) {
 
@@ -27,63 +24,57 @@ function createColRow(nbrRow, nbrCol) {
             let col = document.createElement('td');
             col.setAttribute('class', 'col-td');
             col.classList.add('col-td');
-            // let img = document.createElement('img');
-            // col.innerHTML = `<img src="./img/fleche.png" alt="img">`
             row.appendChild(col);
-
         }
         table.appendChild(row);
     }
 }
 
-function chekInArray(item, array) {
-    for (let i = 0; i < array.length; i++) {
-        if (item === array[i]) {
-            return true;
+createColRow(nbrRow, nbrCol)
+
+// choose picture
+function choosePix(imgArray, arraySave) {
+    while (arraySave.length < 3) {
+        let imgRand = imgArray[Number(Math.floor(Math.random() * imgArray.length))];
+
+        if (!arraySave.includes(imgRand)) {
+            arraySave.push(imgRand);
         }
     }
-    return false;
+    return arraySave;
 }
 
-function deleteItem(imgRand, imgArray) {
-    for (var i = imgArray.length - 1; i >= 0; i--) {
-        if (imgArray[i] === ingRand) {
-            imgArray.splice(i, 1);
-        }
+console.warn("save :", arraySave)
+let img = choosePix(imgArray, arraySave)
+
+//delete if exist
+function removeItemOnce(arr, item) {
+    let index = arr.indexOf(item);
+    if (index > -1) {
+        arr.splice(index, 1);
     }
+    return arr;
 }
 
-function choosePix(imgArray) {
-    let arraySave = [];
+//show pix
+function showPix(arraySave) {
+    let listCopy = [];
+    let listImg = arraySave;
+
     document.querySelectorAll('.col-td').forEach(td => {
-            let imgRand = imgArray[Number(Math.floor(Math.random() * imgArray.length))];
-        td.innerHTML = `<img src="${imgRand}" alt="img">` ;
-        !chekInArray(imgRand, arraySave) ? arraySave.push(imgRand) : deleteItem(imgRand, imgArray);
+
+        let numberRandom = Number(Math.floor(Math.random() * listImg.length));
+        let img = listImg[numberRandom];
+        let alt = img.split('/');
+        //.at() methode qui permet de recupérer un element d'un tableau en fonction de son indice
+        td.innerHTML = `<img src="${img}" alt="${alt.at(-1)}">`;
+
+        if (listCopy.includes(img)) {
+            removeItemOnce(listImg, img);
+            listImg = removeItemOnce(listImg, img);
+        } else {
+            listCopy.push(img);
+        }
     })
 }
-function showImg(img) {
-
-}
-
-function checkSameItem(img) {
-    img.forEach()
-}
-
-function play(nbrRow, nbrCol, img) {
-    // createColRow(nbrRow, nbrCol);
-
-    document.querySelector('.btnPlay').addEventListener("click", function (e){
-        choosePix(img)
-            e.preventDefault();
-        })
-
-    // cont >= 1 ? choosePix(img) : img.style.display = 'none' ;
-    // img.style.display = 'none'
-
-    // document.querySelector('.col-td').addEventListener("click", function (e){
-    //     console.log("click")
-    //     e.preventDefault();
-    // })
-
-
-}
+showPix(arraySave)
