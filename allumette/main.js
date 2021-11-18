abc96c56b49e0c06d014f57756a7ddbaac881968
Ylenia <ylenia.icmr@gmail.com>
2021-11-18 14:54:42 +0100

    remove Stick

--- allumette/main.js
+++ allumette/main.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     InsertImgAllumette("IdNbrAllumette", "ShowNbrAllumette");
+    removeMatchStick();
 });
 
-let divNbrAllumette =  document.querySelector(".choiceNbrAllumette");
+let divNbrAllumette = document.querySelector(".choiceNbrAllumette");
 let divShowStick = document.getElementById("ShowNbrAllumette");
+const divNbrRestant = document.querySelector('.nbrAllumetteDeleted');
 
 function InsertImgAllumette(nbrAllumette, divAllum) {
     let input = document.getElementById("IdNbrAllumette");
@@ -13,48 +15,62 @@ function InsertImgAllumette(nbrAllumette, divAllum) {
     input.addEventListener('input', (event) => {
         // this.value;
         // console.log( event.target)
-        let  nbr = event.target.value;
+        let nbr = event.target.value;
         clear(divShowStick)
         console.log(nbr)
 
-       if ((nbr >= 5 ) && (nbr <= 20)){
-           for (let i = 0; i <nbr ; i++) {
-               // const addAllum =  document.getElementById("idNbre").innerHTML ="gd"
-               const img = document.createElement('img');
-               img.src = "img/matchstick.png";
-               img.alt = "MatchStick_" + i;
-               divShowStick.appendChild(img)
-           }
-       }
-      
-        
+        if ((nbr >= 5) && (nbr <= 20)) {
+            for (let i = 0; i < nbr; i++) {
+                // const addAllum =  document.getElementById("idNbre").innerHTML ="gd"
+                const img = document.createElement('img');
+                img.src = "img/matchstick.png";
+                img.alt = "MatchStick_" + i;
+                divShowStick.appendChild(img)
+            }
+        }
+
+
     });
 }
+
 //reset
 function clear(divShowStick) {
     divShowStick.innerHTML = "";
 }
+
 function play() {
 
 
 }
 
-function createSelectBtn(){
+function createSelectBtn() {
     var select = document.createElement("select");
     select.name = "pets";
     select.id = "pets"
 }
 
-function removeMatchStick(){
-    // input.addEventListener('input', (event) => {
-    //     // this.value;
-    //     // console.log( event.target)
-    //     let  nbrStickDelete = event.target.value;
-    //     clear(divShowStick)
-    //     console.log(nbr)
-    //
-    //     if (nbrStickDelete >=1 && nbr <=3){
-    //     div.}
-    // }
+function removeMatchStick() {
+
+    let inpuToNbrDelete = document.querySelector('.nbrToDelete');
+
+    inpuToNbrDelete.addEventListener('input', (event) => {
+
+        let nbrStickDelete = event.target.value;
+        console.log(nbrStickDelete)
+
+
+        // // clear(divShowStick)
+        //
+        if (nbrStickDelete >= 1 && nbrStickDelete <= 3) {
+            for (let i = 0; i < nbrStickDelete; i++) {
+                divShowStick.removeChild(divShowStick.childNodes[i]);
+            }
+
+
+        }
+
+
+    })
 
 }
+
