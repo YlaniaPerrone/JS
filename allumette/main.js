801c169d28d30d010a11b7eafac857bdf854464f
Ylenia <ylenia.icmr@gmail.com>
2021-11-18 11:05:58 +0100

    add input

--- allumette/main.js
+++ allumette/main.js
@@ -3,33 +3,39 @@ document.addEventListener("DOMContentLoaded", function() {
     InsertImgAllumette("IdNbrAllumette", "ShowNbrAllumette");
 });
 
+let divNbrAllumette =  document.querySelector(".choiceNbrAllumette");
+let divShowStick = document.getElementById("ShowNbrAllumette");
+
 function InsertImgAllumette(nbrAllumette, divAllum) {
     let input = document.getElementById("IdNbrAllumette");
-    let div = document.getElementById("ShowNbrAllumette");
 
 
     input.addEventListener('input', (event) => {
         // this.value;
         // console.log( event.target)
-       let  nbr = event.target.value;
-       removeMatchStick(div)
+        let  nbr = event.target.value;
+        clear(divShowStick)
         console.log(nbr)
 
-        for (let i = 0; i <nbr ; i++) {
-            // const addAllum =  document.getElementById("idNbre").innerHTML ="gd"
-            const img = document.createElement('img');
-            img.src = "img/matchstick.png";
-            img.alt = "MatchStick_" + i;
-            div.appendChild(img)
-        }
+       if ((nbr >= 5 ) && (nbr <= 20)){
+           for (let i = 0; i <nbr ; i++) {
+               // const addAllum =  document.getElementById("idNbre").innerHTML ="gd"
+               const img = document.createElement('img');
+               img.src = "img/matchstick.png";
+               img.alt = "MatchStick_" + i;
+               divShowStick.appendChild(img)
+           }
+       }
+      
+        
     });
 }
 //reset
-function removeMatchStick(div) {
-    div.innerHTML = "";
+function clear(divShowStick) {
+    divShowStick.innerHTML = "";
 }
-
 function play() {
+
 
 }
 
@@ -39,6 +45,16 @@ function createSelectBtn(){
     select.id = "pets"
 }
 
-function deleteAlluumette(){
+function removeMatchStick(){
+    // input.addEventListener('input', (event) => {
+    //     // this.value;
+    //     // console.log( event.target)
+    //     let  nbrStickDelete = event.target.value;
+    //     clear(divShowStick)
+    //     console.log(nbr)
+    //
+    //     if (nbrStickDelete >=1 && nbr <=3){
+    //     div.}
+    // }
 
 }
